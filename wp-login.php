6c6840a54de962f5980ad743cb99257f2b49e0ba
timmose <timmose.mssg@gmail.com>
2020-05-11 18:26:45 +0300

    Fixing CSS

--- wp-login.php
+++ wp-login.php
@@ -9,11 +9,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Войти &lsaquo; Sergey Glushan &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='/https://timmose.github.io/website/wp-includes/css/dashicons.min.css?ver=5.4.1' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='/https://timmose.github.io/website/wp-includes/css/buttons.min.css?ver=5.4.1' media='all' />
-<link rel='stylesheet' id='forms-css'  href='/https://timmose.github.io/website/wp-admin/css/forms.min.css?ver=5.4.1' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='/https://timmose.github.io/website/wp-admin/css/l10n.min.css?ver=5.4.1' media='all' />
-<link rel='stylesheet' id='login-css'  href='/https://timmose.github.io/website/wp-admin/css/login.min.css?ver=5.4.1' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='http://localhost/https:/timmose.github.io/website/wp-includes/css/dashicons.min.css?ver=5.4.1' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='http://localhost/https:/timmose.github.io/website/wp-includes/css/buttons.min.css?ver=5.4.1' media='all' />
+<link rel='stylesheet' id='forms-css'  href='http://localhost/https:/timmose.github.io/website/wp-admin/css/forms.min.css?ver=5.4.1' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='http://localhost/https:/timmose.github.io/website/wp-admin/css/l10n.min.css?ver=5.4.1' media='all' />
+<link rel='stylesheet' id='login-css'  href='http://localhost/https:/timmose.github.io/website/wp-admin/css/login.min.css?ver=5.4.1' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -25,7 +25,7 @@
 		<div id="login">
 		<h1><a href="https://ru.wordpress.org/">Сайт работает на WordPress</a></h1>
 	
-		<form name="loginform" id="loginform" action="/https://timmose.github.io/website/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="http://localhost/https:/timmose.github.io/website/wp-login.php" method="post">
 			<p>
 				<label for="user_login">Имя пользователя или email</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
@@ -49,34 +49,34 @@
 		</form>
 
 					<p id="nav">
-									<a href="/https://timmose.github.io/website/wp-login.php?action=lostpassword">Забыли пароль?</a>
+									<a href="http://localhost/https:/timmose.github.io/website/wp-login.php?action=lostpassword">Забыли пароль?</a>
 								</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
-				<p id="backtoblog"><a href="/https://timmose.github.io/website/">
+				<p id="backtoblog"><a href="http://localhost/https:/timmose.github.io/website/">
 		&larr; Назад к сайту &laquo;Sergey Glushan&raquo;		</a></p>
 			</div>
-	<script src='/https://timmose.github.io/website/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
-<script src='/https://timmose.github.io/website/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+	<script src='http://localhost/https:/timmose.github.io/website/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
+<script src='http://localhost/https:/timmose.github.io/website/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
 <script>
 var _zxcvbnSettings = {"src":"\/wp-includes\/js\/zxcvbn.min.js"};
 </script>
-<script src='/https://timmose.github.io/website/wp-includes/js/zxcvbn-async.min.js?ver=1.0'></script>
+<script src='http://localhost/https:/timmose.github.io/website/wp-includes/js/zxcvbn-async.min.js?ver=1.0'></script>
 <script>
 var pwsL10n = {"unknown":"\u041d\u0430\u0434\u0451\u0436\u043d\u043e\u0441\u0442\u044c \u043f\u0430\u0440\u043e\u043b\u044f \u043d\u0435\u0438\u0437\u0432\u0435\u0441\u0442\u043d\u0430","short":"\u041e\u0447\u0435\u043d\u044c \u0441\u043b\u0430\u0431\u044b\u0439","bad":"\u0421\u043b\u0430\u0431\u044b\u0439","good":"\u0421\u0440\u0435\u0434\u043d\u0438\u0439","strong":"\u041d\u0430\u0434\u0451\u0436\u043d\u044b\u0439","mismatch":"\u041d\u0435\u0441\u043e\u0432\u043f\u0430\u0434\u0435\u043d\u0438\u0435"};
 </script>
-<script src='/https://timmose.github.io/website/wp-admin/js/password-strength-meter.min.js?ver=5.4.1'></script>
-<script src='/https://timmose.github.io/website/wp-includes/js/underscore.min.js?ver=1.8.3'></script>
+<script src='http://localhost/https:/timmose.github.io/website/wp-admin/js/password-strength-meter.min.js?ver=5.4.1'></script>
+<script src='http://localhost/https:/timmose.github.io/website/wp-includes/js/underscore.min.js?ver=1.8.3'></script>
 <script>
 var _wpUtilSettings = {"ajax":{"url":"\/website\/wp-admin\/admin-ajax.php"}};
 </script>
-<script src='/https://timmose.github.io/website/wp-includes/js/wp-util.min.js?ver=5.4.1'></script>
+<script src='http://localhost/https:/timmose.github.io/website/wp-includes/js/wp-util.min.js?ver=5.4.1'></script>
 <script>
 var userProfileL10n = {"warn":"\u0412\u0430\u0448 \u043d\u043e\u0432\u044b\u0439 \u043f\u0430\u0440\u043e\u043b\u044c \u043d\u0435 \u0431\u044b\u043b \u0441\u043e\u0445\u0440\u0430\u043d\u0451\u043d.","warnWeak":"\u0420\u0430\u0437\u0440\u0435\u0448\u0438\u0442\u044c \u0438\u0441\u043f\u043e\u043b\u044c\u0437\u043e\u0432\u0430\u043d\u0438\u0435 \u0441\u043b\u0430\u0431\u043e\u0433\u043e \u043f\u0430\u0440\u043e\u043b\u044f.","show":"\u041f\u043e\u043a\u0430\u0437\u0430\u0442\u044c","hide":"\u0421\u043a\u0440\u044b\u0442\u044c","cancel":"\u041e\u0442\u043c\u0435\u043d\u0430","ariaShow":"\u041f\u043e\u043a\u0430\u0437\u0430\u0442\u044c \u043f\u0430\u0440\u043e\u043b\u044c","ariaHide":"\u0421\u043a\u0440\u044b\u0442\u044c \u043f\u0430\u0440\u043e\u043b\u044c"};
 </script>
-<script src='/https://timmose.github.io/website/wp-admin/js/user-profile.min.js?ver=5.4.1'></script>
+<script src='http://localhost/https:/timmose.github.io/website/wp-admin/js/user-profile.min.js?ver=5.4.1'></script>
 	<script>
 	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
 	</script>
